cf01ad30b2c9dcabb9c3164cd736d431ca914126
Sivaram <sivaram@cradlewise.com>
2024-01-31 16:35:20 +0530

    arm64-install.sh: Add sym links

--- arm64-install.sh
+++ arm64-install.sh
@@ -11,4 +11,9 @@ sudo mkdir -p /usr/include
 
 sudo cp -r binaries/arm64/include/* /usr/include
 
+# make required symbolic links
+
+sudo ln -s /usr/lib/librnnoise.so.0.4.1 /usr/lib/librnnoise.so.0
+sudo ln -s /usr/lib/librnnoise.so.0.4.1 /usr/lib/librnnoise.so
+
 echo "Done"
